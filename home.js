866e390bc61efb72d205361e75a9b243e219f26d
JoniJo06 <jonasjuergens6@gmail.com>
2021-11-03 15:48:00 +0100

    change es5 syntax to es6

--- home.js
+++ home.js
@@ -15,11 +15,10 @@ const items = JSON.parse(localStorage.getItem('items')) || [];
 
 window.onload = () => {
 	populateList(items, itemsList);
-	addItem();
+	handleToDos();
 };
 
-document.querySelector('#add-todo-btn-submit').onclick = addItem;
-function addItem() {
+const handleToDos = () => {
 	if (document.querySelector('#todo-list-text-input').value.length === 0) {
 	} else {
 		// document.getElementById('todo-list').innerHTML += `
@@ -51,17 +50,17 @@ function addItem() {
 	const current_tasks = document.querySelectorAll('.delete');
 
 	for (let i = 0; i < current_tasks.length; i++) {
-		current_tasks[i].onclick = () => {
+		current_tasks[i].addEventListener('click', () => {
 			current_tasks[i].parentNode.remove();
 			items.splice(i, 1);
 			localStorage.setItem('items', JSON.stringify(items));
-		};
+		});
 	}
 
 	const checkbox = document.querySelectorAll('.todo-checkbox');
 
 	for (let i = 0; i < checkbox.length; i++) {
-		checkbox[i].onclick = () => {
+		checkbox[i].addEventListener('click', () => {
 			let item = checkbox[i];
 			let checked = item.classList.contains('fa-check-square');
 			if (checked === true) {
@@ -75,7 +74,7 @@ function addItem() {
 				items[i].done = true;
 				localStorage.setItem('items', JSON.stringify(items));
 			}
-		};
+		});
 	}
 
 	const taskName = document.querySelectorAll('.task-name');
@@ -83,7 +82,7 @@ function addItem() {
 	const editField = document.querySelectorAll('.edit-field');
 
 	for (let i = 0; i < editTask.length; i++) {
-		editTask[i].onclick = () => {
+		editTask[i].addEventListener('click', () => {
 			let item = taskName[i];
 			let edit = item.classList.contains('edit');
 
@@ -102,23 +101,26 @@ function addItem() {
 				editField[i].style.display = 'block';
 				editField[i].value = taskName[i].textContent;
 			}
-		};
+		});
 	}
-}
-function populateList(plates = [], platesList) {
-	platesList.innerHTML = plates
-		.map((plate, i) => {
+};
+
+document.querySelector('#add-todo-btn-submit').addEventListener('click', handleToDos);#
+
+function populateList(todos = [], todoList) {
+	todoList.innerHTML = todos
+		.map((todo, i) => {
 			return `
-	<div class="mdl-list__item todo-list-item">
-	<span class="mdl-list__item-primary-content">
-	<i class="far ${plate.done ? 'fa-check-square' : 'fa-square'} fa-square todo-checkbox"></i>
-	<label id="item${i}" class="task-name">${plate.text}</label>
-	<input type="text" class="edit-field"></input>
-	</span>
-	<div class="mdl-list__item-secondary-action edit"><i class="far fa-edit"></i></div>
-	<div class="mdl-list__item-secondary-action color-red delete"><i class="fas fa-trash-alt"></i></div>
-	</div>
-	  `;
+		<div class="mdl-list__item todo-list-item">
+		<span class="mdl-list__item-primary-content">
+		<i class="far ${todo.done ? 'fa-check-square' : 'fa-square'} fa-square todo-checkbox"></i>
+		<label id="item${i}" class="task-name">${todo.text}</label>
+		<input type="text" class="edit-field"></input>
+		</span>
+		<div class="mdl-list__item-secondary-action edit"><i class="far fa-edit"></i></div>
+		<div class="mdl-list__item-secondary-action color-red delete"><i class="fas fa-trash-alt"></i></div>
+		</div>
+		`;
 		})
 		.join('');
 }
@@ -135,3 +137,4 @@ addToDoBtn.addEventListener('click', () => {
 		showToDoInput = true;
 	}
 });
+// change es5 syntax to es6
